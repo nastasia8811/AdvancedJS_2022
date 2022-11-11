9b3fa794bcd574f14088fbbec46fb95e925e1ccc
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-11 13:47:20 +0100

    не geibnm

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
-//import {Login} from "./classes/classLogin.js";
-import {myLogin} from "./api/myLogin";
-import {getCards} from "./api/getCards.js";
-import {createCards} from "./api/createCard.js";
+// import {myLogin} from "./api/myLogin.js";
+// import {getCards} from "./api/getCards.js";
+// import {createCards} from "./api/createCard.js";
+ import {myLogin} from "./classes/classLogin.js";
 
-
-myLogin()
-getCards()
-createCards()
+// const header = document.body.querySelector('.header')
+// console.log(header)
+// header.insertAdjacentHTML('beforeend',`<div class="logo">LOGO</div><button class="button">Вхід</button>` )
+// const button = document.body.querySelector('.button')
+// button.addEventListener("click", () => {
+//
+// })
 
