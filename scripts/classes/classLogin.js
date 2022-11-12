6a59170b4c39e62a36b0bab04729663a799b7c8b
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-12 12:01:54 +0100

    api

--- scripts/classes/classLogin.js
+++ scripts/classes/classLogin.js
@@ -1,9 +1,9 @@
-
 export class MyLogin {
     constructor(email, password) {
         this.email = email;
         this.password = password;
     }
+
     request() {
         return fetch("https://ajax.test-danit.com/api/v2/cards/login", {
             method: "POST",
@@ -14,8 +14,12 @@ export class MyLogin {
                 email: this.email,
                 password: this.password,
             }),
+        }).then(res => {
+            console.log(res.text())
         })
-
+            .then(token => {
+                console.log(token)
+            })
     }
 }
 
