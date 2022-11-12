e9cbc230aea4f64d43de954ad02875b6a7959b4e
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-12 18:16:36 +0100

    api

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 import {Authorization} from "./classes/classModalAuthorization.js";
-import {header,headerButton, bodyContainer} from "./constanta.js";
+//import {header,headerButton, bodyContainer} from "./constanta.js";
 
 // import {getCards} from "./api/getCards.js";
 // import {createCards} from "./api/createCard.js";
 
+export const header = document.body.querySelector('.header')
+export const headerButton = document.body.querySelector('.header_button')
+export const bodyContainer = document.body.querySelector('.body_container')
 
-
-header.insertAdjacentHTML('beforeend',`<div class="logo">LOGO</div><button class="header_button">Вхід</button>` );
+header.insertAdjacentHTML('beforeend',`<div class="logo">LOGO</div><button class="header_button">Вхід</button>`);
 headerButton.addEventListener("click", () => {
    new Authorization().createElements();
 })
