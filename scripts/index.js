0930dc8c7828322e39b92e285199e7faa845a5ae
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-14 22:25:45 +0100

    modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import {Authorization} from "./classes/classModalAuthorization.js";
 import {Modal} from "./classes/modal.js";
 import {changeButton} from "./constanta.js";
-import {createCards} from "./api/createCard";
+//import {createCards} from "./api/createCard";
 
 const headerButton = document.body.querySelector('.header_button')
 headerButton.addEventListener("click", () => {
@@ -12,5 +12,5 @@ changeButton.addEventListener('click', () => {
     new Modal().createElements()
 })
 
-//this.doctor, this.data, this.reason, this.priority, this.clientName
-new Modal(this.doctor, this.data, this.reason, this.priority, this.clientName).createElements()
+
+
