61b204ad037dc84ab8e6efb581b3e91b7cfb5556
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-14 22:00:38 +0100

    modal

--- scripts/api/createCard.js
+++ scripts/api/createCard.js
@@ -1,9 +1,10 @@
 //Мельник
+import {Modal} from '../classes/modal.js'
 export const createCards = async () => {
     try {
         return fetch('https://ajax.test-danit.com/api/v2/cards', {
             method: 'POST',
-            body: JSON.stringify({name: `${this.name}`, doctor:`${this.doctor}`, priority: `${this.priority}`, reason: `${this.reason}`, data: `${this.data}`}),
+            body: JSON.stringify({name: `${this.clientName}`, doctor:`${this.doctor}`, priority: `${this.priority}`, reason: `${this.reason}`, data: `${this.data}`}),
             headers: {
                 'content-type': 'application/json',
                 "authorization": `Bearer ${localStorage.getItem('token')}`
