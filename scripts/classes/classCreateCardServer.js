4e2a7cba400f5053d130202c8b7ae1ecf83bcd58
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-16 19:20:00 +0100

    create cards+modal

--- scripts/classes/classCreateCardServer.js
+++ scripts/classes/classCreateCardServer.js
@@ -1,10 +1,9 @@
 //Мельник
 export class CreateCardServer {
-    constructor(name, doctor, urgency, purpose, data, description, id) {
+    constructor(name, doctor, urgency, purpose, description, id) {
         this.name = name;
         this.doctor = doctor;
         this.purpose = purpose;
-        this.data = data;
         this.urgency = urgency;
         this.description = description;
         this.id = id;
@@ -13,7 +12,7 @@ export class CreateCardServer {
     requestCreateCard(){
         return fetch('https://ajax.test-danit.com/api/v2/cards', {
             method: 'POST',
-            body: JSON.stringify({name: `${this.name}`, doctor:`${this.doctor}`, urgency: `${this.urgency}`, purpose: `${this.purpose}`, data: `${this.data}`, description:`${this.description}`, id:`${this.id}`}),
+            body: JSON.stringify({name: `${this.name}`, doctor:`${this.doctor}`, urgency: `${this.urgency}`, purpose: `${this.purpose}`, description:`${this.description}`, id:`${this.id}`}),
             headers: {
                 'content-type': 'application/json',
                 "authorization": `Bearer ${localStorage.getItem('token')}`
