0e5e17136731a7fa95e7b8b237bbea1a07591951
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-16 16:10:18 +0100

    create cards+modal

--- scripts/api/getCards.js
+++ scripts/api/getCards.js
@@ -1,18 +1,30 @@
 //Мельник
+import {filter} from "../functions/filterFunction.js";
+
 export const getCards = async () => {
-    if (localStorage.getItem('token')) {
-        fetch('https://ajax.test-danit.com/api/v2/cards', {
-            headers: {
-                'Content-Type': 'application/json',
-                "authorization": `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-            .then(res => res.json())
-            .then(data => console.log(data))
-    } else {
-        document.body.innerHTML = 'PLS LOGIN'
+if (localStorage.getItem('token')) {
+    fetch('https://ajax.test-danit.com/api/v2/cards', {
+        headers: {
+            'Content-Type': 'application/json',
+            "authorization": `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+} else {
+    document.body.innerHTML = 'PLS LOGIN'
+}
+const filter = (arr) =>{
+    return arr.filter((item) =>{
+        const valueDoctor = document.getElementById('search_doctor').value
+        if (valueDoctor !== "виберіть лікаря"){
+
+        }
     }
 
+)
 }
+}
+
 
 
