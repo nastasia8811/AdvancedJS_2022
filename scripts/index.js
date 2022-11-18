a2d59ed2b09afc7d985b3db076e944df1cabbe68
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-18 23:23:07 +0100

    Input+ create card

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 import {Authorization} from "./classes/classModalAuthorization.js";
+//import {Modal} from "./classes/modal.js";
 import {Modal} from "./classes/1modal.js";
 import {bodyContainer, changeButton} from "./constanta.js";
-//import {Filter} from "./classes/classFilter.js";
-
+import {filter, renderList} from "./functions/filterFunction.js";
+import {Filter} from "./classes/classFilter.js";
+//import {createCards} from "./api/createCard.js";
 
 const headerButton = document.body.querySelector('.header_button')
 headerButton.addEventListener("click", () => {
@@ -13,5 +15,5 @@ changeButton.addEventListener('click', () => {
     new Modal().createElements()
 })
 
-//new Filter().filterApear()
+new Filter().filterApear()
 
