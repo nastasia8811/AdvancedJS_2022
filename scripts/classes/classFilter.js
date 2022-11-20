b2ad56c9f866fa26993fa9695990bd3600a66d97
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-20 20:25:15 +0100

    filter

--- scripts/classes/classFilter.js
+++ scripts/classes/classFilter.js
@@ -15,13 +15,13 @@ export class Filter {
 <input class="filter_doctor" type="text" id="search_doctor" placeholder="Введіть назву лікаря (Cardiologist, Dentist, Therapist)">
     <ul id="results_filter_doctor">
     </ul>
-    <div class="dropdownStatus">
-  <button class="dropbtnStatus">Status</button>
-  <div id="myDropdownStatus" class="dropdown-content-status">
-    <a class ='selectDone' href="#">Done</a>
-    <a class ='selectOpen' href="#">Open</a>
-  </div>
-</div>
+
+<select class="visit_status">
+<option value="All">All</option>
+<option value="Done">Done</option>
+<option value="Open">Open</option>
+</select>
+                        
 <input class="filter_priority" type="text" id="search_priority" placeholder="Введіть терміновість візиту (High, Normal, Low)">
     <ul id="results_filter_priority">
     </ul>
@@ -29,78 +29,43 @@ export class Filter {
 
         //-----------------------------------------------------------------
 
-        const dropbtnStatus = document.querySelector('.dropbtnStatus')
-        dropbtnStatus.addEventListener('click', (event) => {
-            document.getElementById("myDropdownStatus").classList.toggle("show");
-            if (!event.target.matches('.dropbtnStatus')) {
-                const dropdownContentStatus = document.querySelector(".dropdown-content-status");
-                for (let i = 0; i < dropdownContentStatus.length; i++) {
-                    const openDropdownStatus = dropdownContentStatus[i];
-                    if (openDropdownStatus.classList.contains('show')) {
-                        openDropdownStatus.classList.remove('show');
-                    }
-                }
-            }
-        })
-        const selectDone = document.querySelector('.selectDone')
-        const selectOpen = document.querySelector('.selectOpen')
-
-
-        //-----------------------------------------------------------------
-
-
-        let filterDoctor = ['Cardiologist', 'Dentist', 'Therapist']
-        let filterStatus = ['Open', 'Done',]
-        let filterPriority = ['High', 'Normal', 'Low']
         const doctorInput = document.querySelector('.filter_doctor')
-        //const statusInput = document.querySelector('.filter_doctor')
-        // const resultsFilterDoctor = document.getElementById('results_filter_doctor')
-        // const resultsFilterStatus = document.getElementById('results_filter_status')
-        // const resultsFilterPriority = document.getElementById('results_filter_priority')
+        const visitStatus = document.querySelector('.visit_status')
 
-        // renderList(filterDoctor, resultsFilterDoctor)
-        // renderList(filterStatus, resultsFilterStatus)
-        // renderList(filterPriority, resultsFilterPriority)
         //-----------------------------------------------------------------
+
         document.querySelector('.filter_container').addEventListener('input', (event) => {
-
             let result = this.arr;
-            if (event.target === doctorInput && event.target.value !== "") {
-                console.log(result.filter(elem => {
-                    return elem.doctor.includes(event.target.value)
+            if (doctorInput.value !== "") {
+                result = result.filter(elem => {
+                    return elem.doctor.includes(doctorInput.value)
 
-                }))
-            }
-            else if (event.target === dropbtnStatus){
-                const arrStatus = [selectDone, selectOpen]
-            document.getElementById("myDropdownStatus").addEventListener('click', (event) => {
-                arrStatus.forEach((elem) => {
-                    elem.addEventListener('click', (event) => {
-                        if (elem === selectDone) {
-                            let today = new Date()
-                            let dd = elem.date.slice(0, 2);
-                            let mm = elem.date.slice(3, 5);
-                            let yyyy = elem.date.slice(6);
-                            let date = today.getFullYear() - yyyy;
-                            if (mm > today.getMonth() || (mm === today.getMonth() && dd > today.getDay())) {
-                             result.filter(elem => {
-                                 return elem.date.includes(event.target.value)
-                            }
-                        } else {
-
-                        }
-
-                        })
-                    })
                 })
             }
 
+            if (visitStatus.value !== 'All') {
+                result = result.filter(elem => {
+                    let currentDateMs = new Date(elem.date).getTime()
+                    let todayDateMs = new Date().getTime()
+                    if(currentDateMs > todayDateMs && visitStatus.value === 'Open'){
+            return true
+                    }
+else if (todayDateMs > currentDateMs && visitStatus.value === 'Done'){
+                        return true
+                    }
+                })
+                console.log(result)
+
+                        }
+                //     })
+                // })
+                // } else {
+                    // console.log(result.filter(elem => {
+                    //     return elem.urgency.includes(event.target.value)
+            //     }))
+            // }
+
+
         })
-        //-----------------------------------------------------------------
-        // document.getElementById('search_doctor').addEventListener('input', e => renderList(filter(e.target.value, filterDoctor), resultsFilterDoctor))
-        // document.getElementById('search_status').addEventListener('input', e => renderList(filter(e.target.value, filterStatus), resultsFilterStatus))
-        // document.getElementById('search_priority').addEventListener('input', e => renderList(filter(e.target.value, filterPriority), resultsFilterPriority))
-
-
     }
 }
