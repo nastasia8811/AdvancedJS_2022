e158ef8b15df077c38d4212b0c980ba8c973a3db
Melnyk Anastasiia <nastasia8811@gmail.com>
2022-11-22 12:21:34 +0100

    filter

--- scripts/classes/classFilter.js
+++ scripts/classes/classFilter.js
@@ -1,9 +1,5 @@
 //Мельник
 import {bodyContainer} from "../constanta.js";
-//import {filter, renderList} from "../functions/filterFunction.js";
-// import {InputCardiologist} from "./classInputCardiologist.js";
-// import {InputTherapist} from "./classInputTherapist.js";
-// import {InputDentist} from "./classInputDentist.js";
 
 export class Filter {
     constructor(arr) {
@@ -12,34 +8,31 @@ export class Filter {
 
     filterApear() {
         bodyContainer.insertAdjacentHTML('beforeend', `<div class="filter_container">
-<input class="filter_doctor" type="text" id="search_doctor" placeholder="Введіть назву лікаря (Cardiologist, Dentist, Therapist)">
+        <input class="filter_doctor" type="text" id="search_doctor" placeholder="Введіть назву лікаря (Cardiologist, Dentist, Therapist)">
     <ul id="results_filter_doctor">
     </ul>
 
-<select class="visit_status">
-<option value="All">All</option>
-<option value="Done">Done</option>
-<option value="Open">Open</option>
-</select>
+    <select class="visit_status">
+    <option value="All">All</option>
+    <option value="Done">Done</option>
+    <option value="Open">Open</option>
+    </select>
                         
-<input class="filter_priority" type="text" id="search_priority" placeholder="Введіть терміновість візиту (High, Normal, Low)">
+        <input class="filter_priority" type="text" id="search_priority" placeholder="Введіть терміновість візиту (High, Normal, Low)">
     <ul id="results_filter_priority">
     </ul>
-</div>`);
+    </div>`);
 
-        //-----------------------------------------------------------------
 
         const doctorInput = document.querySelector('.filter_doctor')
         const visitStatus = document.querySelector('.visit_status')
-
-        //-----------------------------------------------------------------
+        const urgencyStatus = document.querySelector('.filter_priority')
 
         document.querySelector('.filter_container').addEventListener('input', (event) => {
             let result = this.arr;
             if (doctorInput.value !== "") {
                 result = result.filter(elem => {
                     return elem.doctor.includes(doctorInput.value)
-
                 })
             }
 
@@ -47,21 +40,25 @@ export class Filter {
                 result = result.filter(elem => {
                     let currentDateMs = new Date(elem.date).getTime()
                     let todayDateMs = new Date().getTime()
-                    if(currentDateMs > todayDateMs && visitStatus.value === 'Open'){
-            return true
-                    }
-else if (todayDateMs > currentDateMs && visitStatus.value === 'Done'){
+                    if (currentDateMs > todayDateMs && visitStatus.value === 'Open') {
+                        return true
+                    } else if (todayDateMs > currentDateMs && visitStatus.value === 'Done') {
                         return true
                     }
                 })
                 console.log(result)
+            }
+if (urgencyStatus.value !== ""){
+    result = result.filter(elem => {
+        return elem.urgency.includes(urgencyStatus.value)
+    })
+}
 
-                        }
-                //     })
-                // })
-                // } else {
-                    // console.log(result.filter(elem => {
-                    //     return elem.urgency.includes(event.target.value)
+            //     })
+            // })
+            // } else {
+            // console.log(result.filter(elem => {
+            //     return elem.urgency.includes(event.target.value)
             //     }))
             // }
 
