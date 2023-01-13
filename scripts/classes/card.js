bef3104c6607557d2d7df584286c89ba4b85cb65
anastasiiamelnyk <nastasia8811@gmail.com>
2023-01-13 14:57:01 +0100

    styles

--- scripts/classes/card.js
+++ scripts/classes/card.js
@@ -10,6 +10,7 @@ export class Card {
         this.id = id;
 
         this.date = date;
+        this.containerCards = document.createElement('div');
         this.container = document.createElement('div');
         this.changeVisit = document.createElement("button");
         this.deleteButton = document.createElement("button");
@@ -19,22 +20,25 @@ export class Card {
         this.container.dataset.date = date;
     }
 
-    createElements(parent= document.body) {
-        parent.after(this.container);
-        this.container.insertAdjacentHTML('beforeend', `<h1 class="card_name">Patient name: ${this.name}</h1><h3 class="card_doctor">Selected doctor: ${this.doctor}</h3>`);
+    createElements(parent= document.body.querySelector('.cards_container')) {
+        parent.append(this.container);
+        this.container.insertAdjacentHTML('beforeend', `<h1 class="card_name card_row">Patient name: ${this.name}</h1><h3 class="card_doctor card_row">Selected doctor: ${this.doctor}</h3>`);
         this.container.className = `card`;
-        this.deleteButton.innerHTML = "Delete card";
+        this.deleteButton.innerHTML = "X";
+        this.deleteButton.className="deleteCard btn btn-outline-danger";
         this.container.append(this.changeVisit);
         this.changeVisit.innerHTML = "Change visit";
+        this.changeVisit.className="changeVisit btn btn-outline-success";
         this.container.append(this.deleteButton);
         this.container.append(this.buttonShowMore);
         this.buttonShowMore.innerHTML = "Show more";
+        this.buttonShowMore.className="buttonShowMore btn btn-outline-success";
         this.changeVisit.addEventListener("click",() => {
             new Modal( this.name, this.doctor, this.purpose, this.urgency, this.description, this.date, this.id).changeCard()
         })
 
         this.buttonShowMore.addEventListener("click", () => {
-            this.container.insertAdjacentHTML('beforeend', `<h1 class="card_purpose">Purpose: ${this.purpose}</h1><h3 class="card_urgency">Urgency: ${this.urgency}</h3><p class="card_description">Description: ${this.description}</p> <p class="date">Date: ${this.date}</p>`);
+            this.container.insertAdjacentHTML('beforeend', `<h1 class="card_purpose card_row">Purpose: ${this.purpose}</h1><h3 class="card_urgency card_row">Urgency: ${this.urgency}</h3><p class="card_description card_row">Description: ${this.description}</p> <p class="date card_row">Date: ${this.date}</p>`);
         })
         this.deleteButton.addEventListener("click", () => {
             deleteCard(this.id, this.container)
